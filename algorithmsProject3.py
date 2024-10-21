e2b5904f9a99e33d6ef3500829241228c3fb3ca0
acrson <cstell@cub.uca.edu>
2024-10-21 11:31:16 -0500

    Initial Commit
    This is the file that project 3 will be done on. Other testing may be done on separate files, but this one will implement everything into a final product.

---
+++ algorithmsProject3.py
@@ -0,0 +1,14 @@
+"""
+This program allows the user to experiment with different graph-related algorithms
+(algorithms) and their implications.
+
+Fathia Tafesh
+Sera Yang
+Carson Stell
+"""
+
+def main():
+    print("Temporary line")
+    
+if __name__ == "__main__":
+    main()
